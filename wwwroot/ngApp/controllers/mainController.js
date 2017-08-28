5b7f97679a945c9ab97627f0442d7667b88b01d3
Drewski84 <drew@bronnenmail.com>
2017-08-28 14:58:01 -0700

    made mainController.js changes

--- wwwroot/ngApp/controllers/mainController.js
+++ wwwroot/ngApp/controllers/mainController.js
@@ -5,17 +5,13 @@ class MainController {
 }
 
 
-    }
-}
-
-fortune = ["terribleness", "a thousand curses", "something that could be... better I guess.", "... enter your credit card # to continue.", "something that's not so bad", "$0.25", "utter greatness", "a bright future ahead of you or finding one half of a granola bar on the ground when you need it most.", "a lifetime supply of carpet."]
+fortune = ["terribleness.", "a thousand curses.", "something that could be... better I guess.", "... enter your credit card # to continue.", "something that's not so bad.", "$0.25 sucka!", "utter greatness.", "a bright future ahead of you or finding one half of a granola bar on the ground when you need it most.", "a lifetime supply of carpet."]
 
 function getFortune(input) {
     let result = "";
        result= input[(Math.floor((Math.random()* input.length)))];
        return result;
     }
-
 
 
 // Create return random fortune
