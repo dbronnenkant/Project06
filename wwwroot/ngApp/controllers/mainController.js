4b932d450f88777d916c70de78a750a2cf2d4b9c
Maiphuong Pham <you@ git config --global user.email maipham@ucdavis.edu>
2017-08-28 14:40:36 -0700

    My commit

--- wwwroot/ngApp/controllers/mainController.js
+++ wwwroot/ngApp/controllers/mainController.js
@@ -1,7 +1,17 @@
 class MainController {
     constructor() {
-        this.message = "hello from main";
+        this.message = getFortune(fortune);
     }
 }
+
+fortune = ["terribleness", "a thousand curses", "something that could be... better I guess.", "... enter your credit card # to continue.", "something that's not so bad", "$0.25", "utter greatness", "a bright future ahead of you or finding one half of a granola bar on the ground when you need it most.", "a lifetime supply of carpet."]
+
+function getFortune(input) {
+    let result = "";
+       result= input[(Math.floor((Math.random()* input.length)))];
+       return result;
+    }
+
+
 
 // Create return random fortune
